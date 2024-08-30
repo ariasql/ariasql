06cee710b113490012106a4977124b0834340732
Alex Gaetano Padula <apadula@cursusdb.com>
2024-08-29 22:13:02 -0400

    more optimizer work

--- src/optimizer/optimizer.go
+++ src/optimizer/optimizer.go
@@ -40,7 +40,6 @@ type PlanCost struct {
 // Optimize optimizes the AST and returns a PhysicalPlan
 // Not every statement needs optimization, so we return the AST as is, the executor will handle the execution of the statement
 func Optimize(ast parser.Node, cat *catalog.Catalog) *PhysicalPlan {
-	log.Printf("optimizing statement: %T", ast)
 
 	switch ast.(type) {
 	case *parser.CreateDatabaseStmt:
@@ -93,7 +92,60 @@ func OpInsertStmt(stmt *parser.InsertStmt, cat *catalog.Catalog) *PhysicalPlan {
 	return &PhysicalPlan{Plan: stmt} // no optimization needed for insert
 }
 
+// SelectPlan is the plan for a select statement
 type SelectPlan struct {
+	IndexScan       *IndexScan       // uses an index to find the relevant rows more quickly than a table scan.
+	TableScan       *TableScan       //  reads the entire table to find the relevant rows.
+	NestedLoopsJoin *NestedLoopsJoin // For each row in the first table, the DBMS scans the entire second table. This is efficient when one of the tables is much smaller than the other.
+	HashJoin        *HashJoin        // builds a hash table of the smaller table, then scans the larger table and uses the hash table to find matching rows. This is efficient when the tables are of similar size.
+	SortMergeJoin   *SortMergeJoin   // sorts both tables on the join column, then merges them together.
+	Materialize     *Materialize     // creates a temporary table in memory to hold and quickly access the results of a subquery.
+}
+
+// UpdatePlan is the plan for an update statement
+type UpdatePlan struct {
+	IndexScan *IndexScan
+	TableScan *TableScan
+}
+
+// DeletePlan is the plan for a delete statement
+type DeletePlan struct {
+	IndexScan *IndexScan
+	TableScan *TableScan
+}
+
+// NestedLoopsJoin is the plan for a nested loops join
+type NestedLoopsJoin struct {
+	LeftPlan  *PhysicalPlan // the left plan
+	RightPlan *PhysicalPlan // the right plan
+}
+
+// HashJoin is the plan for a hash join
+type HashJoin struct {
+	LeftPlan  *PhysicalPlan // the left plan
+	RightPlan *PhysicalPlan // the right plan
+}
+
+// SortMergeJoin is the plan for a sort merge join
+type SortMergeJoin struct {
+	LeftPlan  *PhysicalPlan // the left plan
+	RightPlan *PhysicalPlan // the right plan
+}
+
+// Materialize is the plan for a materialize operation
+type Materialize struct {
+	SubqueryPlan *PhysicalPlan // the plan for the subquery
+}
+
+// TableScan is the plan for a table scan
+type TableScan struct {
+	TableName string
+}
+
+// IndexScan is the plan for an index scan
+type IndexScan struct {
+	IndexName string
+	TableName string
 }
 
 func OpSelectStmt(stmt *parser.SelectStmt, cat *catalog.Catalog) *PhysicalPlan {
@@ -107,6 +159,11 @@ func OpSelectStmt(stmt *parser.SelectStmt, cat *catalog.Catalog) *PhysicalPlan {
 	if stmt.From != nil {
 		// We have a FROM clause
 		// We can have multiple tables in the FROM clause
+		ast, err := parser.PrintAST(stmt.From)
+		if err != nil {
+			return nil
+		}
+		log.Println(ast)
 	}
 
 	//best := getBestPlan(plans)
