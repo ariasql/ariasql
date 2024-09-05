116b249de53c9880d9988d873b58f727020beda2
Alex Gaetano Padula <apadula@cursusdb.com>
2024-09-04 22:44:38 -0400

    New, connect methods for asql struct, implementing cli

--- cli/main.go
+++ cli/main.go
@@ -17,189 +17,199 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
-	term "github.com/nsf/termbox-go"
-	"log"
+	"net"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
 )
 
 const PROMPT = "ariasql>"
 
 // ASQL is the AriaSQL CLI structure
 type ASQL struct {
-	history       []string
-	historyIndex  int
-	signalChannel chan *os.Signal
-	buffer        []rune
+	history       []string        // History of statements
+	historyIndex  int             // Current history index (used for up and down arrow keys)
+	signalChannel chan *os.Signal // Channel to receive OS signals
+	buffer        []rune          // Buffer to store the current input
+	conn          *net.TCPConn    // Connection to the server
+	secureConn    *tls.Conn       // Secure connection to the server
+	addr          *net.TCPAddr    // Address to connect to
+	authenticated bool            // Is the user authenticated?
+}
+
+// New creates a new ASQL instance
+func New() *ASQL {
+	return &ASQL{
+		history:       make([]string, 0),
+		historyIndex:  0,
+		signalChannel: make(chan *os.Signal, 1),
+		buffer:        make([]rune, 0),
+		authenticated: false,
+	}
+}
+
+// Connect connects to the AriaSQL server
+func (a *ASQL) connect(host string, port int, secure bool) error {
+	var err error
+
+	// Resolve the string address to a TCP address
+	a.addr, err = net.ResolveTCPAddr("tcp4", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		return err
+	}
+
+	if secure {
+		// Connect to the server using TLS
+		a.secureConn, err = tls.Dial("tcp", fmt.Sprintf("%s:%d", host, port), &tls.Config{})
+		if err != nil {
+			return err
+		}
+	} else {
+
+		// Connect to the server
+		a.conn, err = net.DialTCP("tcp", nil, a.addr)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
 }
 
 // WIP!
 func main() {
-	history := make([]string, 0)
-	// Create a channel to receive OS signals
-	sigs := make(chan os.Signal, 1)
-
-	// Register the channel to receive specific signals
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	err := term.Init()
-	if err != nil {
-		panic(err)
-	}
-
-	go func() {
-
-		sig := <-sigs
-		switch sig {
-		case syscall.SIGINT:
-			term.Close()
-			// Handling SIGINT (Ctrl+C) signal
-			fmt.Println("\nReceived SIGINT, shutting down...")
-			os.Exit(0)
-		case syscall.SIGTERM:
-			term.Close()
-			// Handling SIGTERM signal
-			fmt.Println("\nReceived SIGTERM, shutting down...")
-			os.Exit(0)
-		}
-	}()
-
-	runeCh := make(chan rune)
-
-	buffer := make([]rune, 0)
-
-	prompt := "ariasql>"
-
-	go func() {
-
-		defer term.Close()
-
-		for {
-			switch ev := term.PollEvent(); ev.Type {
-			case term.EventKey:
-				switch ev.Key {
-				case term.KeyCtrlC:
-					term.Close()
-					sigs <- syscall.SIGINT
-					break
-				case term.KeyEsc:
-					term.Sync()
-				case term.KeyArrowDown:
-					term.Sync()
-				case term.KeyArrowUp:
-					// Get the last item in the history
-					if len(history) > 0 {
-						// Get the last item in the history
-						lastItem := history[len(history)-1]
-
-						// Clear the current buffer
-						buffer = []rune{}
-
-						for i := 0; i < len(prompt); i++ {
-							runeCh <- rune(prompt[i])
-							term.Sync()
-						}
-
-						for _, r := range lastItem {
-							runeCh <- r
-							term.Sync()
-						}
-
-					}
-				case term.KeySpace:
-					runeCh <- ' '
-				case term.KeyBackspace2, term.KeyBackspace:
-					if len(buffer) > len(prompt) {
-						runeCh <- '\b'
-					}
-
-				case term.KeyEnter:
-					if strings.HasSuffix(string(buffer), ";") && !strings.HasSuffix(string(buffer), "\";") && !strings.HasSuffix(string(buffer), "';") {
-						history = append(history, string(buffer[len(prompt):len(buffer)]))
-						buffer = []rune{}
-
-						term.Sync()
-						for i := 0; i < len(prompt); i++ {
-							runeCh <- rune(prompt[i])
-							term.Sync()
-
-						}
-
-					} else {
-						term.Sync()
-						runeCh <- '\n'
-					}
-
-				default:
-					term.Sync()
-					runeCh <- ev.Ch
-
-				}
-			case term.EventError:
-				panic(ev.Err)
-			}
-		}
-	}()
-
-	for i := 0; i < len(prompt); i++ {
-		buffer = append(buffer, rune(prompt[i]))
-
-	}
-
-	for {
-		term.Sync()
-		log.Println(string(buffer))
-		select {
-		case r := <-runeCh:
-			if r == '\b' {
-				buffer = buffer[:len(buffer)-1]
-			} else {
-				buffer = append(buffer, r)
-			}
-		}
-
-	}
-
-	//// Get input from the user
-	//reader := bufio.NewReader(os.Stdin)
+	//history := make([]string, 0)
+	//// Create a channel to receive OS signals
+	//sigs := make(chan os.Signal, 1)
 	//
-	//// Prompt the user
-	//fmt.Print("Enter input (type 'exit' to quit)\n")
-	//fmt.Print("ariasql>")
+	//// Register the channel to receive specific signals
+	//signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	//
+	//err := term.Init()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//go func() {
+	//
+	//	sig := <-sigs
+	//	switch sig {
+	//	case syscall.SIGINT:
+	//		term.Close()
+	//		// Handling SIGINT (Ctrl+C) signal
+	//		fmt.Println("\nReceived SIGINT, shutting down...")
+	//		os.Exit(0)
+	//	case syscall.SIGTERM:
+	//		term.Close()
+	//		// Handling SIGTERM signal
+	//		fmt.Println("\nReceived SIGTERM, shutting down...")
+	//		os.Exit(0)
+	//	}
+	//}()
+	//
+	//runeCh := make(chan rune)
+	//
+	//buffer := make([]rune, 0)
+	//
+	//prompt := "ariasql>"
+	//
+	//go func() {
+	//
+	//	defer term.Close()
+	//
+	//	for {
+	//		switch ev := term.PollEvent(); ev.Type {
+	//		case term.EventKey:
+	//			switch ev.Key {
+	//			case term.KeyCtrlC:
+	//				term.Close()
+	//				sigs <- syscall.SIGINT
+	//				break
+	//			case term.KeyEsc:
+	//				term.Sync()
+	//			case term.KeyArrowDown:
+	//				term.Sync()
+	//			case term.KeyArrowUp:
+	//				// Get the last item in the history
+	//				if len(history) > 0 {
+	//					// Get the last item in the history
+	//					lastItem := history[len(history)-1]
+	//
+	//					// Clear the current buffer
+	//					buffer = []rune{}
+	//
+	//					for i := 0; i < len(prompt); i++ {
+	//						runeCh <- rune(prompt[i])
+	//						term.Sync()
+	//					}
+	//
+	//					for _, r := range lastItem {
+	//						runeCh <- r
+	//						term.Sync()
+	//					}
+	//
+	//				}
+	//			case term.KeySpace:
+	//				runeCh <- ' '
+	//			case term.KeyBackspace2, term.KeyBackspace:
+	//				if len(buffer) > len(prompt) {
+	//					runeCh <- '\b'
+	//				}
+	//
+	//			case term.KeyEnter:
+	//				if strings.HasSuffix(string(buffer), ";") && !strings.HasSuffix(string(buffer), "\";") && !strings.HasSuffix(string(buffer), "';") {
+	//					history = append(history, string(buffer[len(prompt):len(buffer)]))
+	//					buffer = []rune{}
+	//
+	//					term.Sync()
+	//
+	//					// response
+	//					response := []byte("OK\n")
+	//
+	//					for i := 0; i < len(response); i++ {
+	//						runeCh <- rune(response[i])
+	//						term.Sync()
+	//					}
+	//
+	//					for i := 0; i < len(prompt); i++ {
+	//						runeCh <- rune(prompt[i])
+	//						term.Sync()
+	//
+	//					}
+	//
+	//				} else {
+	//					term.Sync()
+	//					runeCh <- '\n'
+	//				}
+	//
+	//			default:
+	//				term.Sync()
+	//				runeCh <- ev.Ch
+	//
+	//			}
+	//		case term.EventError:
+	//			panic(ev.Err)
+	//		}
+	//	}
+	//}()
+	//
+	//for i := 0; i < len(prompt); i++ {
+	//	buffer = append(buffer, rune(prompt[i]))
+	//
+	//}
 	//
 	//for {
-	//	var inputBuilder strings.Builder
-	//	for {
-	//
-	//		// Get input from the user
-	//		// If the user types "exit", break the loop
-	//
-	//		// Read a line of input
-	//		line, err := reader.ReadString('\n')
-	//		if err != nil {
-	//			fmt.Println("Error reading input:", err)
-	//			continue
+	//	term.Sync()
+	//	fmt.Print(string(buffer))
+	//	select {
+	//	case r := <-runeCh:
+	//		if r == '\b' {
+	//			buffer = buffer[:len(buffer)-1]
+	//		} else {
+	//			buffer = append(buffer, r)
 	//		}
-	//
-	//		// Append the line to the inputBuilder
-	//		inputBuilder.WriteString(line)
-	//
-	//		// Check if the line contains a semicolon
-	//		if strings.HasSuffix(line, ";\n") && !strings.HasSuffix(line, "\";\n") && !strings.HasSuffix(line, "';\n") {
-	//			break
-	//		}
-	//
 	//	}
-	//
-	//	// Get the complete input and trim the trailing semicolon and newline
-	//	fullInput := inputBuilder.String()
-	//	fullInput = strings.TrimSpace(fullInput)
-	//	fullInput = strings.TrimSuffix(fullInput, ";")
-	//
-	//	log.Println("Full input:", fullInput)
 	//
 	//}
 
